6383ae75ac5da7cf381571355633dfebb6d68b2b
DavidCrr <david.contato62@gmail.com>
2024-12-20 22:42:37 -0300

    correção do nome do package
    correção do nome do packege e das classes.

--- supermecado/src/main/java/Sistema_Supermercado/Usuarios.java
+++ supermecado/src/main/java/Sistema_Supermercado/Usuarios.java
@@ -1,6 +1,6 @@
-package cadastro;
+package Sistema_Supermercado;
 
-public class ProdutosVencimento {
+public class Usuarios {
     public static void main(String[] args) {
         System.out.println(25);
     }
