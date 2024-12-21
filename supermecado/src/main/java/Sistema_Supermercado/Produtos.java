577735c3fd49750df2afd52b1de657b4577461cd
DavidCrr <david.contato62@gmail.com>
2024-12-20 22:45:41 -0300

    criação da classe cliente
    criei os principais atributos e métodos da classe cliente, método construtor , métodos get e set, vou analisar a necessidade de alteração do toString

---
+++ supermecado/src/main/java/Sistema_Supermercado/Produtos.java
@@ -0,0 +1,15 @@
+package Sistema_Supermercado;
+
+public class Produtos {
+    //atributos da classe
+    private String nome;
+    private double preco;
+    private int qtdEstoque;
+    
+    //metodo construtor
+    public Produtos(String nome, double preco, int qtdEstoque){
+    this.nome =  nome;
+    this.preco = preco;
+    this.qtdEstoque = qtdEstoque;
+    }
+}
